26969c0dafc3d877b00101d6a149284f6000cb1a
Asad <asadkhakimov05@gmail.com>
2023-08-19 21:10:11 +0600

    add

--- js/index.js
+++ js/index.js
@@ -84,3 +84,9 @@ var addTwoNumbers = function (l1, l2) {
 };
 
 console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
+
+
+function squareSum(numbers) {
+  return Math.pow(+numbers);
+}
+console.log(squareSum(3));
